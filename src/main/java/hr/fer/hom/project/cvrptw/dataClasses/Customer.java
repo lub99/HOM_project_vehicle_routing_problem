d1d432590e737a639fb92af663b3ea0e767d5a33
lub99 <matej.luburic@gmail.com>
2022-01-04 22:51:27 +0100

    Added Customer fields.

--- src/main/java/hr/fer/hom/project/cvrptw/dataClasses/Customer.java
+++ src/main/java/hr/fer/hom/project/cvrptw/dataClasses/Customer.java
@@ -1,4 +1,39 @@
 package hr.fer.hom.project.cvrptw.dataClasses;
 
 public class Customer {
+    private Integer customerIndex;
+    private Integer xCoordinate;
+    private Integer yCoordinate;
+    private Integer demand;
+    private Integer readyTime;
+    private Integer dueDate;
+    private Integer serviceTime;
+
+    public Integer getCustomerIndex() {
+        return customerIndex;
+    }
+
+    public Integer getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public Integer getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public Integer getDemand() {
+        return demand;
+    }
+
+    public Integer getReadyTime() {
+        return readyTime;
+    }
+
+    public Integer getDueDate() {
+        return dueDate;
+    }
+
+    public Integer getServiceTime() {
+        return serviceTime;
+    }
 }
