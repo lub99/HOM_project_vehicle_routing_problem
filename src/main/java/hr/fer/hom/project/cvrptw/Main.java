48c5903aa5e19be34081374e4b8847811c13ee21
Filip Šimek <filip.simek@fer.hr>
2022-01-05 17:36:27 +0100

    proba

--- src/main/java/hr/fer/hom/project/cvrptw/Main.java
+++ src/main/java/hr/fer/hom/project/cvrptw/Main.java
@@ -3,6 +3,6 @@ package hr.fer.hom.project.cvrptw;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        System.out.println("Hello1");
     }
 }
