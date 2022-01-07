32fa89c4d8c524d7a528ea79019958feb3a21696
Filip Šimek <filip.simek@fer.hr>
2022-01-07 12:43:32 +0100

    CustomerCalc razred s atributima arrivalTime i positionOnRoute

--- src/main/java/hr/fer/hom/project/cvrptw/dataClasses/Vehicle.java
+++ src/main/java/hr/fer/hom/project/cvrptw/dataClasses/Vehicle.java
@@ -8,7 +8,8 @@ public class Vehicle {
     private Integer vehicleIndex;
     private Integer remainingCapacity;
     private Integer capacityLimit;
-    private List<Customer> route;
+    //private List<Customer> route;
+    private List<CustomerCalc> route;
     private Double routeLength;
     private Integer routeTime;
 
@@ -17,7 +18,8 @@ public class Vehicle {
         this.capacityLimit = capacityLimit;
         this.remainingCapacity = capacityLimit;
         this.route = new ArrayList<>();
-        this.route.add(depot);
+        CustomerCalc d = new CustomerCalc(depot, 0, 0.0);
+        this.route.add(d);
         this.routeTime = 0;
         this.routeLength = 0.0;
     }
@@ -44,12 +46,16 @@ public class Vehicle {
         return routeTime;
     }
 
-    public List<Customer> getRoute() {
+    public List<CustomerCalc> getRoute() {
         return route;
     }
 
-    public Customer getLastCustomerInRoute() {
+    public CustomerCalc getLastCustomerCalcInRoute() {
         return route.get(route.size()-1);
+    }
+
+    public Customer getLastCustomerInRoute() {
+        return route.get(route.size()-1).getCustomer();
     }
 
     public void setRouteTime(int time) {
@@ -60,31 +66,36 @@ public class Vehicle {
     }
 
     public void addCustomerToEnd(Customer c, double[][] distances){
-        Customer lastCustomer = getLastCustomerInRoute();
-        this.route.add(c);
+        CustomerCalc lastCustomer = getLastCustomerCalcInRoute();
+        //CustomerCalc nextCustomer = new CustomerCalc(c);
         this.remainingCapacity -= c.getDemand();
         this.setRouteLength(calculateRoutePositionOfNextCustomer(lastCustomer, c, distances));
         this.setRouteTime(calculateArrivalTimeToNextCustomer(lastCustomer, c, distances));
-        c.setArrivalTime(this.getRouteTime());
-        c.setPositionOnRoute(this.getRouteLength());
+        //c.setArrivalTime(this.getRouteTime());
+        //c.setPositionOnRoute(this.getRouteLength());
         c.setServed(true);
+        CustomerCalc nextCustomer = new CustomerCalc(c);
+        nextCustomer.setArrivalTime(this.getRouteTime());
+        nextCustomer.setPositionOnRoute(this.getRouteLength());
+        this.route.add(nextCustomer);
     }
 
-    public double calculateRoutePositionOfNextCustomer(Customer previousCustomer,
+    public double calculateRoutePositionOfNextCustomer(CustomerCalc previousCustomer,
                                                      Customer nextCustomer, double[][] distances){
         return previousCustomer.getPositionOnRoute()
-                + distances[previousCustomer.getCustomerIndex()][nextCustomer.getCustomerIndex()];
+                + distances[previousCustomer.getCustomer().getCustomerIndex()][nextCustomer.getCustomerIndex()];
     }
 
-    public int calculateArrivalTimeToNextCustomer(Customer previousCustomer,
+    public int calculateArrivalTimeToNextCustomer(CustomerCalc previousCustomer,
                                                  Customer nextCustomer, double[][] distances){
         return Math.max(nextCustomer.getReadyTime(), previousCustomer.getArrivalTime()
-                + previousCustomer.getServiceTime()
-                + (int)(distances[previousCustomer.getCustomerIndex()][nextCustomer.getCustomerIndex()]+1));
+                + previousCustomer.getCustomer().getServiceTime()
+                + (int)(distances[previousCustomer.getCustomer().getCustomerIndex()][nextCustomer.getCustomerIndex()]+1));
     }
 
     public void returnToGarage(double[][] distances) {
-        Customer depot = this.route.get(0).copy();
+        //Customer depot = this.route.get(0).copy();
+        Customer depot = this.route.get(0).getCustomer();
         addCustomerToEnd(depot, distances);
     }
 }
